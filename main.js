70b10d8f36a9d7fffdbfcad8162fce119a09bce5
sakshi <whitesky5672@gmail.com>
2021-07-03 15:10:49 +0530

    some new changes

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// Video
 document.addEventListener("DOMContentLoaded", function(){
     document.getElementById("myvideo").style.display = "none";
 })
@@ -19,6 +20,21 @@ document.addEventListener("DOMContentLoaded", function(){
             myvideo.style.display = "none";
             document.getElementById("deskimage").style.display = "block";
             videobtn.innerText = "Watch Video";
+        }
+    }
+})
+
+// Light and Dark mode
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelector("#switch").onclick = function() {
+        if (document.querySelector("#switchinput").checked == true) { 
+            console.log("dark");
+            document.querySelector("body").classList.add("darkmode");
+            document.querySelectorAll(".cardone").classList.add("darkmode");
+        }
+        else {
+            document.querySelector("body").classList.remove("darkmode");
+            document.querySelectorAll(".cardone").classList.remove("darkmode");
         }
     }
 })
