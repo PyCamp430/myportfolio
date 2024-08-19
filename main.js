f1bf2b20a7607d0c478e26b74bd29f610415834e
white <whitesky5672@gmail.com>
2024-08-19 20:18:13 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,25 +1,43 @@
 // Video
-document.addEventListener("DOMContentLoaded", function(){
-    document.getElementById("myvideo").style.display = "none";
-})
 
 document.addEventListener("DOMContentLoaded", function(){
-    document.getElementById("videobtn").onclick = function() {
-        var myvideo = document.getElementById("myvideo");
-        var videobtn = document.getElementById("videobtn");
+    document.querySelector("#videobtn1").onclick = function() {
+        var myvideo1 = document.querySelector("#myvideo1");
+        var videobtn1 = document.querySelector("#videobtn1");
         // play video
-        if(videobtn.innerText == "Play Video"){
-            videobtn.innerText = "Hide Video";
-            document.getElementById("deskimage").style.display = "none";
-            myvideo.style.display = "block";
-            myvideo.play();
+        if(videobtn1.innerText == "Play Video"){
+            videobtn1.innerText = "Hide Video";
+            document.querySelector("#deskImage").style.display = "none";
+            myvideo1.style.display = "block";
+            myvideo1.play();
         }
         else {
         // stop video
-            myvideo.pause();
-            myvideo.style.display = "none";
-            document.getElementById("deskimage").style.display = "block";
-            videobtn.innerText = "Play Video";
+            myvideo1.pause();
+            myvideo1.style.display = "none";
+            document.querySelector("#deskImage").style.display = "block";
+            videobtn1.innerText = "Play Video";
+        }
+    }
+})
+
+document.addEventListener("DOMContentLoaded", function(){
+    document.querySelector("#videobtn2").onclick = function() {
+        var myvideo2 = document.querySelector("#myvideo2");
+        var videobtn2 = document.querySelector("#videobtn2");
+        // play video
+        if(videobtn2.innerText == "Play Video"){
+            videobtn2.innerText = "Hide Video";
+            document.querySelector("#restImage").style.display = "none";
+            myvideo2.style.display = "block";
+            myvideo2.play();
+        }
+        else {
+        // stop video
+            myvideo2.pause();
+            myvideo2.style.display = "none";
+            document.querySelector("#restImage").style.display = "block";
+            videobtn2.innerText = "Play Video";
         }
     }
 })
